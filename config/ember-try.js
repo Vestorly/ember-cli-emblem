a3373be38351d7ec3e173592061585a35e5cec04
Jeremy Ward <jeremy.ward@mckesson.com>
2019-01-10 08:11:29 -0700

    update ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,21 +1,32 @@
+'use strict';
+
 module.exports = {
+  useYarn: true,
   scenarios: [
     {
-      name: 'default',
+      name: 'ember-default',
       dependencies: { }
     },
     {
       name: 'ember-lts-2.12',
+      env: {
+        EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
+      },
       npm: {
         devDependencies: {
+          '@ember/jquery': '^0.5.1',
           'ember-source': '~2.12.0'
         }
       }
     },
     {
-      name: 'Ember 2.18.0',
+      name: 'ember-lts-2.18',
+      env: {
+        EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
+      },
       npm: {
         devDependencies: {
+          '@ember/jquery': '^0.5.1',
           'ember-source': '~2.18.0'
         }
       }
